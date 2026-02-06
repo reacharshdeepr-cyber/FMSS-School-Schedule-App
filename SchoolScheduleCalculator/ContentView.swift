c9044e24459568adb31ac5cdeb8a29e07e1ff981
Arshdeep Rakhra <reach.arshdeepr@gmail.com>
2026-02-06 18:55:34 -0500

    Initial Commit

---
+++ SchoolScheduleCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SchoolScheduleCalculator
+//
+//  Created by Arshdeep Singh Rakhra on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchoolScheduleCalculator/SchoolScheduleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolScheduleCalculatorApp.swift
+//  SchoolScheduleCalculator
+//
+//  Created by Arshdeep Singh Rakhra on 2026-02-06.
+//
+
+import SwiftUI
+
+@main
+struct SchoolScheduleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
